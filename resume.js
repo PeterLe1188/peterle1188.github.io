df4d33260ecd2518f2229c720696963813f65336
Peter Le <peterle@peters-mbp.lan>
2022-03-24 14:17:00 -0500

    new resume html

---
+++ resume.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+	const button = document.querySelector('#menu-button'); // Hamburger Icon
+	const menu = document.querySelector('#menu'); // Menu
+
+	button.addEventListener('click', () => {
+		menu.classList.toggle('hidden');
+	});
+}
